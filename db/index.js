2ec2972fb05d337ef9cc994dc700888be5754d37
yaogengzhu <yaogengzhu@tencent.com>
2023-05-22 19:48:16 +0800

    feat: 引入knex mysql2

---
+++ db/index.js
@@ -0,0 +1,19 @@
+const knex = require("knex");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { MYSQL_HOST, MYSQL_POST, MYSQL_USER, MYSQL_DATABASE, MYSQL_PASSWORD } =
+  process.env;
+
+const db = knex({
+  client: "mysql2",
+  connection: {
+    host: MYSQL_HOST,
+    port: MYSQL_POST,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+  },
+});
+
+module.exports = db;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("../db/index");
 
-router.get("/user", (req, res) => {});
+router.get("/user", async (req, res) => {
+  const images = await knex.select("*").from("image");
+  res.send({
+    code: 200,
+    list: images,
+  });
+});
 
 module.exports = router;
