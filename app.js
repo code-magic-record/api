83c7fe3f8a41c6c908d57d18422700dc46ddd132
yaogengzhu <yaogengzhu@tencent.com>
2023-05-24 10:30:02 +0800

    feat: 添加日志中间件

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
-const morgan = require("morgan");
 const routes = require("./routes/index");
+const { loggerMiddleware } = require("./middleware/log/index");
 
 const app = express();
 
 app.use(bodyParser.json()); // 解析json
 app.use(cookieParser()); // 使用cookie
-app.use(morgan("short")); // 使用日志
+app.use(loggerMiddleware); // 使用日志
 
 app.use("/", routes);
 
-app.get('/', (req, res) => {
-  res.send('server is running')
-})
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
---
+++ middleware/log/index.js
@@ -0,0 +1,52 @@
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
+const onFinished = require("on-finished");
+const path = require("path");
+
+// 日志输出目录
+const logDir = path.resolve(__dirname, "../..", "logs");
+
+// winston 格式化器
+const logFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf((info) => {
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
+);
+
+// 设置 dailyRotateFile
+const dailyRotateFileTransport = new DailyRotateFile({
+  filename: `${logDir}/%DATE%.log`,
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "15d",
+});
+
+// 创建 logger 实例
+const logger = winston.createLogger({
+  level: "info",
+  format: logFormat,
+  transports: [dailyRotateFileTransport],
+});
+
+// 日志中间件
+const loggerMiddleware = (req, res, next) => {
+  // 记录请求的相关信息
+  logger.info(`[${req.method}] ${req.url} - ${req.ip}`);
+
+  // 设置响应时间
+  const start = Date.now();
+  onFinished(res, () => {
+    const durationInMilliseconds = Date.now() - start;
+    logger.info(
+      `[${req.method}] ${req.url} - ${
+        res.statusCode
+      } - ${durationInMilliseconds.toLocaleString()} ms`
+    );
+  });
+
+  next();
+};
+
+module.exports = { logger, loggerMiddleware };
