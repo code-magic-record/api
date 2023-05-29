66af792993a71ab86383bcf94316457a427768a6
yaogengzhu <yaogengzhu@tencent.com>
2023-05-29 09:58:32 +0800

    feat: 用户订单列表api

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const userRouter = require("./user/user");
 const userAddressRouter = require("./user/user_address");
 const goodsRouter = require("./goods/goods");
+const orderRouter = require("./order/order");
 
 router.use("/user", [userRouter, userAddressRouter]); // 注入用户模块, 用户地址模块
 router.use("/goods", [goodsRouter]); // 注入goods模块
+router.use("/order", [orderRouter]);
 
 module.exports = router;
