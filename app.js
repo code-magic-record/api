822a11d2f044812a15106222a73654255b0acb19
yaogengzhu <455947455@qq.com>
2023-06-03 19:24:39 +0800

    chore: swagger实现API文档

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const swaggerJSDoc = require('swagger-jsdoc'); // 接口文档
+const swaggerUi = require('swagger-ui-express'); // 接口文档UI
 const routes = require('./routes/index');
 const { loggerMiddleware } = require('./middleware/loggerMiddleware');
 
 const app = express();
+
+// Swagger 配置选项
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'API 文档',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8001',
+      },
+    ],
+  },
+
+  apis: ['./routes/user/user.js'], // 指定包含路由定义的文件路径
+};
+
+// 生成 Swagger 规范
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(bodyParser.json()); // 解析json
 app.use(cookieParser()); // 使用cookie
@@ -12,8 +36,9 @@ app.use(loggerMiddleware); // 使用日志
 
 app.use('/api/', routes);
 
-app.get('/', async (req, res) => {
-  res.send('server is running ');
+app.use('/', (_, res) => {
+  const dom = `<a href="/api-docs">api文档</a>`;
+  res.send(dom);
 });
 
 app.listen(8001, () => {
--- routes/user/user.js
+++ routes/user/user.js
@@ -8,14 +8,101 @@ const {
 } = require('../../middleware/validationMiddleware');
 
 // 登录/注册校验
-const userVaildator = [
+const userValidator = [
   body('username').isString().withMessage('用户名字段异常'),
   body('password').isString().withMessage('密码'),
 ];
 
-router.post('/register', validationMiddleware(userVaildator), service.register);
-router.post('/login', validationMiddleware(userVaildator), service.login);
+/**
+ * @swagger
+ * /api/user/post:
+ *   post:
+ *     summary: 用户注册
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: body
+ *         name: credentials
+ *         description: 用户名和密码
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *               description: 用户名
+ *             password:
+ *               type: string
+ *               description: 密码
+ *     responses:
+ *       200:
+ *         description: 注册成功
+ *       0:
+ *         description: 注册失败
+ */
+router.post('/register', validationMiddleware(userValidator), service.register);
+
+/**
+ * @swagger
+ * /api/user/login:
+ *   post:
+ *     summary: 用户登录
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: body
+ *         name: credentials
+ *         description: 用户名和密码
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *               description: 用户名
+ *             password:
+ *               type: string
+ *               description: 密码
+ *     responses:
+ *       200:
+ *         description: 登录成功
+ *       400:
+ *         description: 登录失败
+ */
+router.post('/login', validationMiddleware(userValidator), service.login);
+
+/**
+ * @swagger
+ * /api/user/logout:
+ *   post:
+ *     summary: 用户登出
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: 登出成功
+ *       401:
+ *         description: 未授权访问
+ */
 router.post('/logout', authMiddleware, service.logout);
+
+/**
+ * @swagger
+ * /api/user/userInfo:
+ *   get:
+ *     summary: 获取用户信息
+ *     tags:
+ *       - User
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       200:
+ *         description: 成功获取用户信息
+ *       401:
+ *         description: 未授权访问
+ */
 router.get('/userInfo', authMiddleware, service.userInfo);
 
 module.exports = router;
